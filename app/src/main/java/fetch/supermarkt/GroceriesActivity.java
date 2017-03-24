2a7238c5256d331a6474cd0b2add4698dfdf133f
i340824 <s.delaat@student.fontys.nl>
2017-03-24 13:18:47 +0100

    Loading multiple Activities with BaseActivity

---
+++ app/src/main/java/fetch/supermarkt/GroceriesActivity.java
@@ -0,0 +1,20 @@
+package fetch.supermarkt;
+
+import android.os.Bundle;
+
+/**
+ * Created by sande on 24/03/2017.
+ */
+
+public class GroceriesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_groceries;
+    }
+}

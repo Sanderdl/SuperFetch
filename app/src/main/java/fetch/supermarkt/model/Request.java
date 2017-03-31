4a02666702efebbef6ce73c8d729c19bb9772381
Stef Gootzen <stefgootzen@live.nl>
2017-03-31 13:52:23 +0200

    Added firebase upload to request class

--- app/src/main/java/fetch/supermarkt/model/Request.java
+++ app/src/main/java/fetch/supermarkt/model/Request.java
@@ -1,22 +1,35 @@
 package fetch.supermarkt.model;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by sande on 30/03/2017.
  */
 
 public class Request {
+    private String requestId;
     private int productCount;
     private double worth;
-    private double earnings;
+    private double deliveryFee;
     private String location;
-    private String userName;
+    private String requesterName;
+    private String store;
+    private String delivererName;
+    private boolean atSupermarket;
+    private boolean orderReceived;
+    private boolean orderDelivered;
 
-    public Request(int productCount, double worth, double earnings, String location, String userName) {
+
+    public Request(int productCount, double worth, double deliveryFee, String location, String requesterName, String store) {
         this.productCount = productCount;
         this.worth = worth;
-        this.earnings = earnings;
+        this.deliveryFee = deliveryFee;
         this.location = location;
-        this.userName = userName;
+        this.requesterName = requesterName;
+        this.store = store;
+
+        addRequestToFirebase(this);
     }
 
     public int getProductCount() {
@@ -27,15 +40,42 @@ public class Request {
         return worth;
     }
 
-    public double getEarnings() {
-        return earnings;
+    public double getDeliveryFee() {
+        return deliveryFee;
     }
 
     public String getLocation() {
         return location;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getRequesterName() {
+        return requesterName;
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public void addRequestToFirebase(Request r) {
+        //Create Firebase reference
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        //Use push() to generate an unique key and insert this key in the database
+        DatabaseReference mRef = database.getReference().child("requests").push();
+
+        //Get this key and assign it to the object
+        r.setRequestId(mRef.getKey());
+
+        //Insert the object data in Firebase
+        mRef.setValue(r);
+
     }
 }

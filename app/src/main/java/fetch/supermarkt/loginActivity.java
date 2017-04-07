407e45377efb139833f05542bbe56502ba18716c
Stef Gootzen <stefgootzen@live.nl>
2017-04-07 11:17:09 +0200

    FInished login screen

--- app/src/main/java/fetch/supermarkt/loginActivity.java
+++ app/src/main/java/fetch/supermarkt/loginActivity.java
@@ -2,6 +2,7 @@ package fetch.supermarkt;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -14,33 +15,40 @@ import android.widget.Toast;
  */
 
 public class loginActivity extends Activity {
-    Button mButtonLogin;
-    Button mButtonCheck;
-    EditText mEdit;
+    private Button mButtonLogin;
+    private EditText mEdit;
+
+    public static String applicationUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        mButtonLogin = (Button)findViewById(R.id.button_login);
-        mButtonCheck = (Button)findViewById(R.id.button_Check);
-        mEdit   = (EditText)findViewById(R.id.text_username);
-
+        mButtonLogin = (Button) findViewById(R.id.button_login);
+        mEdit = (EditText) findViewById(R.id.text_username);
 
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 writeToSharedPreferences(mEdit.getText().toString());
+                checkIfUsernameIsSet();
             }
         });
 
-        mButtonCheck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                readFromSharedPreferences();
-            }
-        });
+        checkIfUsernameIsSet();
+    }
+
+    private void checkIfUsernameIsSet() {
+        String username = readFromSharedPreferences();
+        if (!username.equals("usernameZettenIsMislukt") && username != null && !username.isEmpty()) {
+            applicationUser = username;
+            Intent myIntent = new Intent(this, MainActivity.class);
+            this.startActivity(myIntent);
+        } else {
+            Toast.makeText(getActivity(), "Please fill in your username",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void writeToSharedPreferences(String username) {
@@ -50,16 +58,19 @@ public class loginActivity extends Activity {
         editor.commit();
     }
 
-    private void readFromSharedPreferences(){
+    private String readFromSharedPreferences() {
         SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-        String defaultValue = "mislukt";
+        String defaultValue = "usernameZettenIsMislukt";
         String username = sharedPref.getString(getString(R.string.user_name), defaultValue);
-
-
-        Toast.makeText(getActivity(), username,
-                Toast.LENGTH_LONG).show();
+        return username;
     }
-    
+
+    private void deleteSharedPreferences() {
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.clear().apply();
+    }
+
     public Activity getActivity() {
         return this;
     }

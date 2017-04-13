eb50071d12e9fa7da01191c8e3562a8aacbc967b
Stef Gootzen <stefgootzen@live.nl>
2017-04-13 12:33:01 +0200

    Updated login screen visually Can't return to login screen on clicking back anymore

--- app/src/main/java/fetch/supermarkt/loginActivity.java
+++ app/src/main/java/fetch/supermarkt/loginActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +27,14 @@ public class loginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         mButtonLogin = (Button) findViewById(R.id.button_login);
         mEdit = (EditText) findViewById(R.id.text_username);
 
@@ -36,6 +46,7 @@ public class loginActivity extends Activity {
             }
         });
 
+        deleteSharedPreferences();
         checkIfUsernameIsSet();
     }
 
